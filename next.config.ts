62789368fba6fc7b8addae57e5e6615afdd51540
suzukiryunosuke <suzuki.ryunosuke.55s@st.kyoto-u.ac.jp>
2025-06-04 23:23:52 +0900

    fix: remove basePath/assetPrefix, export to docs/

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,16 @@
 // next.config.ts
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   //distDir: 'docs',
 
   // 本番環境時 (GitHub Pages のとき) のみ
-  basePath: '',
+  basePath: isProd ? '/cv-website' : '',
   // ここで「/cv-website/_next」をプレフィックスにする
-  assetPrefix: '',
+  assetPrefix: isProd ? '/cv-website' : '',
 };
 
 export default nextConfig;
