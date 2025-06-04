4294ac5b3db92058206e50a5f6a73e7ddca2fe7d
suzukiryunosuke <suzuki.ryunosuke.55s@st.kyoto-u.ac.jp>
2025-06-04 20:04:49 +0900

    Fix ○○

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,8 @@
-// postcss.config.mjs（修正後）
-import tailwind from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
 const config = {
-  plugins: [
-    tailwind(),       // @tailwindcss/postcss を呼び出す
-    autoprefixer(),   // autoprefixer も呼び出す
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
